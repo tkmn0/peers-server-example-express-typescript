047b45e5e1b72662e4feb985bf74a59a0728f278
nzer0 <tkmgin0914@gmail.com>
2020-08-11 01:43:15 +0900

    feat: add environments

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import * as Cors from 'cors';
 import Peers from 'peers-server';
 import * as Path from 'path';
 
-const WEB_DIR = Path.resolve(__dirname, '..', 'web');
+const WEB_DIR = Path.resolve(
+  __dirname,
+  '..',
+  process.env.NODE_ENV === 'development' ? 'web_dev' : 'web'
+);
 const port = process.env.PORT || 3000;
 const app = Express();
 
@@ -12,12 +16,9 @@ app.use(Express.static(WEB_DIR));
 
 // setup peers
 const server = app
-  .use((_, res) =>
-    res.sendFile('index.html', { root: Path.resolve(__dirname, '..', 'web') })
-  )
+  .use((_, res) => res.sendFile('index.html', { root: Path.resolve(WEB_DIR) }))
   .listen(port, () => console.log('server started at', port));
 
 const peers = new Peers(server);
 peers.setLogLevel('info');
 peers.start();
-console.log(process.env.NODE_ENV);
